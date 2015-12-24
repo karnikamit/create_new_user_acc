f4df07154dd99abe436574820bc165f4cc339300
Amit Karnik <amit.karnik@edgenetworks.in>
2015-12-24 12:26:16 +0530

    Add setup.py to make it distrubutable

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = "karnikamit"
+from setuptools import setup
+
+setup(name='create_new_user',
+      version='0.1',
+      description='Creates a new user account and saves the details in MongoDB',
+      url='https://github.com/karnikamit/create_new_user_acc.git',
+      author='karnikamit',
+      author_email='karnikamit46@gmail.com',
+      license='MIT',
+      packages=['create_new_user_acc'],
+      zip_safe=False)
